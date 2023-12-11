f2eca87eaffed1bd548da04b4f6f2292edc0d061
lh2513 <114601271+lh2513@users.noreply.github.com>
2023-12-11 16:59:30 +0800

    Update

--- app/src/main/java/com/github/catvod/spider/Xinsj.java
+++ app/src/main/java/com/github/catvod/spider/Xinsj.java
@@ -115,7 +115,7 @@ public class Xinsj extends Spider {
             Elements sourcesName_el = Jsoup.parse(content).select("[class=module-tab-content]")
                     .select("[class=module-tab-item tab-item]");
             ArrayList<String> playFroms = new ArrayList<String>();
-            Elements sourcesUrl_el = Jsoup.parse(content).select("[class=sort-item]");
+            Elements sourcesUrl_el = Jsoup.parse(content).select(".module-player-list:eq(#id)&&.scroll-content&&a");
             // System.out.println(sourcesUrl_el);
 
             ArrayList<String> play_from_array = new ArrayList<String>();
