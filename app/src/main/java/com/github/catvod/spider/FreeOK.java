4672a4a40adb5ed874592ba9bbbdc2f91393e7a0
lh2513 <114601271+lh2513@users.noreply.github.com>
2023-12-11 11:43:26 +0800

    Update FreeOK.java

--- app/src/main/java/com/github/catvod/spider/FreeOK.java
+++ app/src/main/java/com/github/catvod/spider/FreeOK.java
@@ -44,16 +44,16 @@ public class FreeOK extends Spider {
 
             rebopaihang.put("type_id", "/label/new.html");
             rebopaihang.put("type_name", "热播排行");
-            dianying.put("type_id", "/vodshow/1--------");
+            dianying.put("type_id", "/vod-show/1--------");
             dianying.put("type_name", "电影");
 
-            dianshiju.put("type_id", "/vodshow/2--------");
+            dianshiju.put("type_id", "/vod-show/2--------");
             dianshiju.put("type_name", "电视剧");
 
-            dongman.put("type_id", "/vodshow/3--------");
+            dongman.put("type_id", "/vod-show/3--------");
             dongman.put("type_name", "动漫");
 
-            zongyi.put("type_id", "/vodshow/4--------");
+            zongyi.put("type_id", "/vod-show/4--------");
             zongyi.put("type_name", "综艺");
 
             classes.put(rebopaihang);
